611a8bb854a30927682ea87706e5240431b2fd0c
Victor Caruso <vic.caruso@gmail.com>
2022-05-16 17:25:50 -0700

    removed unused imports pt. 2

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
